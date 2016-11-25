348062d4692cd4b7d2c3f09a0d4595baa75c16a9
Arne Stappen <meh@agoat.de>
2016-11-25 12:46:33 +0100

    Changed old gif icons to svg

--- src/Resources/contao/pattern/PatternVisibility.php
+++ src/Resources/contao/pattern/PatternVisibility.php
@@ -53,7 +53,7 @@ class PatternVisibility extends Pattern
 		{
 			$strPreview .= '<div class="w50 wizard" style="padding-top:10px;"><h3 style="margin: 0;"><label>' . $GLOBALS['TL_LANG']['tl_content_pattern']['start'][0] . '</label></h3>';
 			$strPreview .= '<input class="tl_text" value="" type="text"> ';
-			$strPreview .= '<img src="assets/mootools/datepicker/2.2.0/icon.gif" alt="Page picker" style="vertical-align:-6px;cursor:pointer" height="20" width="20">';
+			$strPreview .= '<img src="assets/datepicker/images/icon.svg" alt="Page picker" style="cursor:pointer" height="20" width="20">';
 			$strPreview .= '<p title="" class="tl_help tl_tip">' . $GLOBALS['TL_LANG']['tl_content_pattern']['start'][1] . '</p></div>';			
 		}
 
@@ -61,7 +61,7 @@ class PatternVisibility extends Pattern
 		{
 			$strPreview .= '<div class="w50 wizard" style="padding-top:10px;"><h3 style="margin: 0;"><label>' . $GLOBALS['TL_LANG']['tl_content_pattern']['stop'][0] . '</label></h3>';
 			$strPreview .= '<input class="tl_text" value="" type="text"> ';
-			$strPreview .= '<img src="assets/mootools/datepicker/2.2.0/icon.gif" alt="Page picker" style="vertical-align:-6px;cursor:pointer" height="20" width="20">';
+			$strPreview .= '<img src="assets/datepicker/images/icon.svg" alt="Page picker" style="cursor:pointer" height="20" width="20">';
 			$strPreview .= '<p title="" class="tl_help tl_tip">' . $GLOBALS['TL_LANG']['tl_content_pattern']['stop'][1] . '</p></div>';	
 		}
 		
