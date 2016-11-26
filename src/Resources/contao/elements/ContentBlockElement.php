c0522d2144409f7e590b9c688f8bf5e880cbf64c
Arne Stappen <meh@agoat.de>
2016-11-26 14:37:10 +0100

    Correcttly use ContentPatternModel

--- src/Resources/contao/elements/ContentBlockElement.php
+++ src/Resources/contao/elements/ContentBlockElement.php
@@ -139,7 +139,7 @@ class ContentBlockElement extends ContentElement
 	protected function compile()
 	{		
 		// get the pattern model collection
-		$colPattern = \ContentPatternModel::findPublishedByPidAndTable($this->objBlock->id, 'tl_content_blocks');
+		$colPattern = \ContentPatternModel::findPublishedByPid($this->objBlock->id);
 
 		if ($colPattern === null)
 		{
