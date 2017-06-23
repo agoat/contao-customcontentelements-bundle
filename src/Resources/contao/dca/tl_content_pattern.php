01f9d981c5992f10e19d05b9fd9c7ced5857adbd
Arne Stappen <meh@agoat.de>
2017-06-23 20:51:15 +0200

    Add Markdown language to PatternCode

--- src/Resources/contao/dca/tl_content_pattern.php
+++ src/Resources/contao/dca/tl_content_pattern.php
@@ -321,7 +321,7 @@ $GLOBALS['TL_DCA']['tl_content_pattern'] = array
 			'label'                   => &$GLOBALS['TL_LANG']['tl_content_pattern']['highlight'],
 			'exclude'                 => true,
 			'inputType'               => 'select',
-			'options'                 => array('HTML', 'HTML5', 'XML', 'JavaScript', 'CSS', 'SCSS', 'PHP', 'JSON'),
+			'options'                 => array('HTML', 'HTML5', 'XML', 'JavaScript', 'CSS', 'SCSS', 'PHP', 'JSON', 'Markdown'),
 			'eval'                    => array('includeBlankOption'=>true, 'tl_class'=>'w50'),
 			'sql'                     => "varchar(32) NOT NULL default ''"
 		),
