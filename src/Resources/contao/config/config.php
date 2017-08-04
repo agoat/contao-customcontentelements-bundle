dbf13ec4ef97267fdd826cd1e869e0a9abd128af
Arne Stappen <meh@agoat.de>
2017-08-04 12:10:17 +0200

    Revert "Merge branch 'release/agoat' into feature/dcaPicker"
    This reverts commit 4f6f7725ca39f9b79239268b9240f1c62d623e3f, reversing
    changes made to 80ca82805773d8c468234012d4bab2f592441706.
    
    	modified:   src/Resources/contao/config/config.php
    	modified:   src/Resources/contao/dca/tl_content_blocks.php
    	modified:   src/Resources/contao/dca/tl_content_pattern.php
    	deleted:    src/Resources/contao/dca/tl_content_subpattern.php
    	deleted:    src/Resources/contao/pattern/PatternMultiPattern.php
    	deleted:    src/Resources/contao/pattern/PatternSubPattern.php
    	deleted:    src/Resources/contao/widgets/MultiGroup.php
    	deleted:    src/Resources/contao/widgets/MultiGroupStart.php
    	deleted:    src/Resources/contao/widgets/MultiGroupStop.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -75,8 +75,6 @@ $GLOBALS['TL_CTP'] = array
 	(
 		'section'		=> 'Agoat\ContentBlocks\PatternSection',
 		'explanation'	=> 'Agoat\ContentBlocks\PatternExplanation',
-		'subpattern'	=> 'Agoat\ContentBlocks\PatternSubPattern',
-		'multipattern'	=> 'Agoat\ContentBlocks\PatternMultiPattern',
 	),
 	'element' => array
 	(
@@ -113,11 +111,6 @@ $GLOBALS['TL_CTP_NA'] = array
 		'module',
 	),
 );
-
-/**
- * Sub pattern
- */
-$GLOBALS['TL_CTP_SUB'] = array('subpattern', 'multipattern');
 
 
 /**
