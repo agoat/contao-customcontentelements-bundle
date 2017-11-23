3c247b109cded8177f9464987e538d342837afab
Arne Stappen <meh@agoat.de>
2017-11-23 09:53:16 +0100

    Add Contao Manager support

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * Custom content elements extension for Contao Open Source CMS.
+ *
+ * @copyright  Arne Stappen (alias aGoat) 2017
+ * @package    contao-contentelements
+ * @author     Arne Stappen <mehh@agoat.xyz>
+ * @link       https://agoat.xyz
+ * @license    LGPL-3.0
+ */
+ 
+namespace Agoat\ContentElementsBundle\ContaoManager;
+
+use Agoat\ContentElementsBundle\AgoatContentElementsBundle;
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
+
+/**
+ * Plugin for the Contao Manager.
+ */
+class Plugin implements BundlePluginInterface
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getBundles(ParserInterface $parser)
+	{
+		return [
+			BundleConfig::create(AgoatContentElementsBundle::class)
+				->setLoadAfter([ContaoCoreBundle::class])
+				->setReplace(['piwikanalytics']),
+		];
+	}
+}
